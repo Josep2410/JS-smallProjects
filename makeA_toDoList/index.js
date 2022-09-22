11f277f04b55d7465257ced73ecfdecfdcd77bac
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-22 16:22:02 -0500

    innerHTML vs textContent notice

--- makeA_toDoList/index.js
+++ makeA_toDoList/index.js
@@ -1,3 +1,6 @@
+//keep in mind innerHTML is susceptible to malicious XSS attacks
+//can bypass by replacing 'innerHTML' with 'textContent'
+
 let numOfEntries = 0;
 document.getElementById("addItem").onclick = addItemToList;
 
