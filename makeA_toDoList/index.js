1903b1c11a4c021a3797cccad349aed43eb71fbb
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-22 15:46:17 -0500

    indented To Do List contents to the left alittle

--- makeA_toDoList/index.js
+++ makeA_toDoList/index.js
@@ -1,16 +1,13 @@
 let numOfEntries = 0;
 document.getElementById("addItem").onclick = addItemToList;
 
-
-
-
 function addItemToList(){
   numOfEntries++;
   
   if (numOfEntries ===1){
-    document.getElementById("to-do-list").innerHTML = document.getElementById("inputBar").value;
+    document.getElementById("to-do-list").innerHTML = "- "+document.getElementById("inputBar").value;
   }
   else if(numOfEntries>1){
-    document.getElementById("to-do-list").innerHTML = document.getElementById("to-do-list").innerHTML + "<br>" + document.getElementById("inputBar").value ;
+    document.getElementById("to-do-list").innerHTML = document.getElementById("to-do-list").innerHTML + "<br>" + "- " +document.getElementById("inputBar").value ;
   }
 }
