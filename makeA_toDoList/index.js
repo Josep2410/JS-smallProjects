5d296bb28aaf898c39b29fc1695af3ca73c6d960
Joseph Moreno <josephpinoy2@yahoo.com>
2022-10-06 21:22:40 -0500

    started submit feature

--- makeA_toDoList/index.js
+++ makeA_toDoList/index.js
@@ -2,7 +2,10 @@
 //can bypass by replacing 'innerHTML' with 'textContent'
 
 const myList = document.querySelector("#to-do-list");
+const submitBTN = document.querySelector("#submitBTN");
+const submitList = document.querySelector("#submittedList")
 
+let numOfEntries = 0;
 
 document.getElementById("addItem").onclick = addItemToList;
 
@@ -13,25 +16,44 @@ document.body.addEventListener('keypress', function(e){
 });
 
 function addItemToList(){
-  const newInput = document.createElement("input");
-  const newLabel = document.createElement("label");
-  const lineFiller = document.createElement("p");
-  newInput.type = 'checkbox';
-  lineFiller.style.margin = 0 + 'px';
+  numOfEntries++;
   
-  newLabel.textContent = document.querySelector("#inputBar").value;
-  
-  myList.prepend(newLabel);
-  myList.prepend(newInput);
-  myList.prepend(lineFiller);
+  if(numOfEntries>9){
+
+  }
+  else{
+    const newInput = document.createElement("input");
+    const newLabel = document.createElement("label");
+    const lineFiller = document.createElement("p");
+    newInput.type = 'checkbox';
+    lineFiller.style.margin = 0 + 'px';
+    
+    newLabel.textContent = document.querySelector("#inputBar").value;
+    
+    myList.prepend(newLabel);
+    myList.prepend(newInput);
+    myList.prepend(lineFiller);
+    
+    const allLI = document.querySelectorAll("li");
+    submitBTN.addEventListener("click" , submitItems);
+    
+  }
+ 
  
 }
 
 function clearList(){
  myList.innerHTML= "";
+ numOfEntries = 0;
 }
 
 function submitItems(){
+  if(confirm("Are you ready to submit items?") == true){
+    window.alert("You said YES");
+  
+  }
+  
+  
 
 }
 
