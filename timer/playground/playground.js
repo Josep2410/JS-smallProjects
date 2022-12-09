842b8255ec4586586e9731793753b9d1ca3cd33e
Joseph Moreno <josephpinoy2@yahoo.com>
2022-12-09 13:59:18 -0600

    adjusted height of display box

---
+++ timer/playground/playground.js
@@ -0,0 +1,10 @@
+
+const img = document.querySelector(".show-img");
+const p = document.querySelector(".p-hide");
+
+window.addEventListener("load", function(){
+  img.classList.add("hide-img");
+  p.classList.add("p-show");
+})
+
+
