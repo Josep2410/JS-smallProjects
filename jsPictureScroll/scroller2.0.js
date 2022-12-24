5bad8e5d54ee0253c9343181c885f8f2b94e32c4
Joseph Moreno <josephpinoy2@yahoo.com>
2022-12-23 23:03:07 -0600

    foundational work before messing w/ animations

---
+++ jsPictureScroll/scroller2.0.js
@@ -0,0 +1,59 @@
+let array = new Array(10);
+const images = [ 
+  {
+    id: 0,
+    img: "images/Aot_enviep.jpg", //images local to my computer
+    title: "attack on titan"
+  }, 
+  {
+    id: 1,
+    img: "images/gojojujutsu.webp",
+    title: "Gojo"
+  }, 
+  {
+    id: 2,
+    img: "images/naruto.jpg",
+    title: "naruto"
+  }, 
+  {
+    id: 3,
+    img: "images/opm.webp",
+    title: "One Punch Man"
+  },
+  {
+    id: 4,
+    img: "images/tarsier.jpg",
+    title: "tarsier"
+  },
+  {
+    id: 5,
+    img: "images/madara.jpg",
+    title: "madara"
+  }, 
+  {
+    id: 6,
+    img: "images/masterRoshi.jpg",
+    title: "masterRoshi"
+  }, 
+  {
+    id: 7 , 
+    img: "images/gojo.PNG", 
+    title: "Gojo"
+  },
+  {
+    id: 8 , 
+    img: "images/tobirama.webp", 
+    title: "tobirama"
+  },
+  {
+    id: 9,
+    img: "images/haikyu.jpg",
+    title: "haikyu"
+  }
+  
+  ]
+
+let randomNum = Math.floor(Math.random()*10); 
+
+
+  
