d76310fdd50f10e87b4bb38e38fa42e1ecf180c6
JDawg123 <josephpinoy2@yahoo.com>
2023-02-06 18:42:53 -0600

    eliminated uncessary code

--- jsLoginScreen/index.js
+++ jsLoginScreen/index.js
@@ -2,15 +2,10 @@ const emailBar = document.querySelector("#email");
 const passwordBar = document.querySelector("#pwd");
 const loginBtn = document.querySelector("#loginBtn");
 
-document.body.addEventListener('keypress', function(e){
-  if(e.key === 'Enter'){
-    console.log(emailBar.value);
-    console.log(passwordBar.value);
-    
-  }
-})
 
 
 loginBtn.addEventListener('click', function(){
+  console.log(emailBar.value);
+  console.log(passwordBar.value);
  document.body.innerHTML = `<p>Logged in \u2713 </p>`;
 })
